cfdc5b2869dad4e0d67374f8ba83649cfbf08df0
javier.gianneo <javier.gianneo@globant.com>
2021-09-22 16:53:45 -0300

    proyecto base

---
+++ ms-supplies/src/main/java/com/workshop/mssupplies/MsSuppliesApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.mssupplies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSuppliesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSuppliesApplication.class, args);
+	}
+
+}
